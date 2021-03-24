a1008ea443cf2d185a90a869efcc4deb04eb70b4
paolotrimini <paolo.trimini@gmail.com>
2021-03-24 14:56:57 +0100

    es 2 - oggetti studenti

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-// - Creare un oggetto che descriva uno studente con le
-// seguenti proprietà: nome, cognome e età. Stampare a
-// schermo attraverso il for in tutte le proprietà.
-// - Creare un array di oggetti di studenti. Ciclare su tutti gli
-// studenti e stampare per ognuno nome e cognome
-// - Dare la possibilità all'utente attraverso 3 prompt di
-// aggiungere un nuovo oggetto studente inserendo
-// nell'ordine: nome, cognome e età.
+
 
 function init() {
     //console.log('test file script.js');
 
-    var studente = {
+// Creare un oggetto che descriva uno studente con le
+// seguenti proprietà: nome, cognome e età. Stampare a
+// schermo attraverso il for in tutte le proprietà.
+
+
+    /*var studente = {
 
         'nome': 'guybrush',
         'cognome': 'threepwood',
@@ -20,6 +18,41 @@ function init() {
     for (var dati in studente) {
 
         console.log(studente[dati]);
+    }*/
+
+
+// Creare un array di oggetti di studenti. Ciclare su tutti gli
+// studenti e stampare per ognuno nome e cognome.
+
+    var studenti = [{
+
+            'nome': 'Paolo',
+            'cognome': 'Verdi'
+        },
+        {
+            'nome': 'Marco',
+            'cognome': 'Rossi'
+        },
+        {
+            'nome': 'Francesca',
+            'cognome': 'Gialli'
+        }
+    ];
+
+    for (var i = 0; i < studenti.length; i++) {
+        var elencoStudenti = studenti[i];
+        console.log(elencoStudenti.nome + " " + elencoStudenti.cognome);
     }
+
+
+// Dare la possibilità all'utente attraverso 3 prompt di
+// aggiungere un nuovo oggetto studente inserendo
+// nell'ordine: nome, cognome e età.
+
+
+
 }
 $(document).ready(init);
+
+
+
